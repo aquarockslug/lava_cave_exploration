1c2c0a286b8df1f26f203e112e886322d92d4db3
lava <zachlava1@gmail.com>
2024-02-08 02:43:28 -0600

    Hallway object

--- cave_exploration.py
+++ cave_exploration.py
@@ -1,66 +1,83 @@
 import pygame
 import sys
+from dataclasses import dataclass
+
+@dataclass
+class GameData():
+    fps: int = 50
+    menu_color: tuple = (0, 255, 0)
+    bg_color: tuple = (255, 255, 255)
+    clock: object = pygame.time.Clock()
+    screen: object = pygame.display.set_mode([1080, 1080])
 
 class Sprite(pygame.sprite.Sprite):
-    def __init__(self, pos):
+    rect = None
+    image = None
+    def __init__(self, pos, size):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface([20, 20])
-        self.image.fill((255, 0, 0))
+        self.image = pygame.Surface([size[0], size[1]])
+        self.image.fill((0, 0, 0))
         self.rect = self.image.get_rect()
-
         self.rect.center = pos
+                
+class Hallway(Sprite):
+    length = 100
+    def __init__(self, pos, size):
+        super().__init__(pos, size)
+        self.length = size[0] 
+
+hall_group = pygame.sprite.Group()
 
 def main():
     pygame.init()
-    clock = pygame.time.Clock()
-    fps = 50
-    bg = [255, 255, 255]
-    size =[480, 480]
+    game = GameData()
 
-    screen = pygame.display.set_mode(size)
-
-    player = Sprite([40, 50])
+    player = Sprite([20, 20], [20, 20])
     player.move = [pygame.K_LEFT, pygame.K_RIGHT, pygame.K_UP, pygame.K_DOWN]
     player.vx = 5
     player.vy = 5
 
-    wall = Sprite([200, 60])
-
-    wall_group = pygame.sprite.Group()
-    wall_group.add(wall)
+    create_paths()
 
     player_group = pygame.sprite.Group()
     player_group.add(player)
 
-    loop = 1
+    loop = True
     while loop:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                loop = 0
+                loop = False
 
-        key = pygame.key.get_pressed()
+        game.screen.fill(game.bg_color)
+        update_player(player)
 
-        for i in range(2):
-            if key[player.move[i]]:
-                player.rect.x += player.vx * [-1, 1][i]
+        # TODO: trigger when player isnt touching hall_group
+        hit = pygame.sprite.spritecollide(player, hall_group, True)
+        if hit: player.image.fill((255, 0, 0))
 
-        for i in range(2):
-            if key[player.move[2:4][i]]:
-                player.rect.y += player.vy * [-1, 1][i]
-
-        screen.fill(bg)
-
-        hit = pygame.sprite.spritecollide(player, wall_group, True)
-
-        if hit: player.image.fill((255, 255, 255))
-
-        player_group.draw(screen)
-        wall_group.draw(screen)
+        player_group.draw(game.screen)
+        hall_group.draw(game.screen)
 
         pygame.display.update()
-        clock.tick(fps)
+        game.clock.tick(game.fps)
 
-    pygame.quit()
+    sys.exit()
 
+def update_player(player):
+    key = pygame.key.get_pressed()
+    for i in range(2):
+        if key[player.move[i]]:
+            player.rect.x += player.vx * [-1, 1][i]
+
+    for i in range(2):
+        if key[player.move[2:4][i]]:
+            player.rect.y += player.vy * [-1, 1][i]
+
+# factory
+def create_paths():
+    # TODO: random generation
+    new_path = Hallway([200, 200], [20, 200])
+    return hall_group.add(new_path)
+    
 if __name__ == '__main__':
     main()
